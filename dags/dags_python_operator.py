b8b675d5ad952597cf13644fd5a984ffca3ca131
jooyong yang <nihonkita17@gmail.com>
2025-07-21 08:19:16 +0900

    add python operator

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -1,13 +1,13 @@
 import pendulum
-# Airflow 3.0 부터 아래 경로로 import 합니다.
-from airflow.operators.python_operator import PythonOperator
-import random
-from airflow.sdk import import pendulum
 import random
 
 # Airflow 3.0 부터 아래 경로로 import 합니다.
 from airflow.sdk import DAG
 from airflow.providers.standard.operators.python import PythonOperator
+
+# Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
+#from airflow.operators.python import PythonOperator
+#from airflow import DAG
 
 
 with DAG(
@@ -23,24 +23,6 @@ with DAG(
 
     py_t1 = PythonOperator(
         task_id='py_t1',
-        python_callable=select_fruit
-    )
-
-    py_t1
-
-with DAG(
-    dag_id="dags_python_operator",
-    schedule="30 6 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
-    catchup=False
-) as dag:
-    def select_fruits():
-        fruit = ['APPLE','BANANA','ORANGE','AVOGADO']
-        rand_int = random.ranint(0,3)
-        print(fruit[rand_int])
-    
-    py_t1 = PythonOperator(
-        task_id = 'py_t1'
         python_callable=select_fruit
     )
 
